1ffd43864da080f434a90c2de2d11906c497096b
cdie <christophe.mommer@gmail.com>
2017-06-02 12:58:29 +0200

    basic extensions tools

---
+++ src/CQELight.Tools/Extensions/ObjectExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQELight.Tools.Extensions
+{
+    /// <summary>
+    /// Class of extensions methods for objects.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+
+        #region Public static methods
+
+        /// <summary>
+        /// Check if type are same.
+        /// </summary>
+        /// <param name="obj">Other objet instance to compare with.</param>
+        /// <param name="value">Object instance.</param>
+        /// <returns>If both object are same type.</returns>
+        public static bool SameTypeCheck(this object value, object obj)
+            => value?.GetType() == obj?.GetType();
+
+
+        #endregion
+
+    }
+}
---
+++ src/CQELight.Tools/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Linq;
+
+namespace CQELight.Tools
+{
+    /// <summary>
+    /// Extension methods for reflection.
+    /// </summary>
+    public static class ReflectionExtensions
+    {
+
+        #region Extension methods
+        
+        /// <summary>
+        /// Retrieve all types of current app.
+        /// </summary>
+        /// <returns>Collection of types.</returns>
+        public static IEnumerable<Type> GetAllTypes(string currentPath)
+        {
+#if NET452
+            var dependencies = AppDomain.CurrentDomain.GetAssemblies();
+            return dependencies.SelectMany(a => a.GetTypes());
+#else
+            var dependencies = DependencyContext.Default.RuntimeLibraries;
+            List<Type> result = new List<Type>();
+            foreach (var library in dependencies.Where(d => d.Type.Equals("project", StringComparison.OrdinalIgnoreCase)))
+            {
+                if (File.Exists(Path.Combine(Path.GetDirectoryName(currentPath), library.RuntimeAssemblyGroups[0].AssetPaths[0])))
+                {
+                    var assembly = Assembly.Load(new AssemblyName(library.Name));
+                    result.AddRange(assembly.GetTypes());
+                }
+
+            }
+            return result;
+#endif
+
+        }
+
+        #endregion
+
+    }
+}
