8e0f333b802b300c250fa11b443f70fbbc2457c5
cdie <christophe.mommer@live.fr>
2017-08-25 20:49:13 +0200

    ef core 2.0

--- src/CQELight.SystemBus/DAL/SystemBusContext.cs
+++ src/CQELight.SystemBus/DAL/SystemBusContext.cs
@@ -29,39 +29,29 @@ namespace CQELight.SystemBus.DAL
             entEnveloppe.ToTable("ETR_T_EVENT_TRANSIT");
             entEnveloppe.HasKey(e => e.Id);
             entEnveloppe.Property(e => e.Sender).IsRequired()
-                .ForSqliteHasColumnName("ETR_SENDER")
-                .ForSqlServerHasColumnName("ETR_SENDER");
+                .HasColumnName("ETR_SENDER");
             entEnveloppe.Property(e => e.Receiver).IsRequired(false)
-                .ForSqliteHasColumnName("ETR_RECEIVER")
-                .ForSqlServerHasColumnName("ETR_RECEIVER");
+                .HasColumnName("ETR_RECEIVER");
             entEnveloppe.Property(e => e.EventTime).IsRequired()
-                .ForSqliteHasColumnName("ETR_TIMESTAMP")
-                .ForSqlServerHasColumnName("ETR_TIMESTAMP");
+                .HasColumnName("ETR_TIMESTAMP");
             entEnveloppe.Property(e => e.EventData).IsRequired()
-                .ForSqliteHasColumnName("ETR_EVENT_DATA")
-                .ForSqlServerHasColumnName("ETR_EVENT_DATA");
+                .HasColumnName("ETR_EVENT_DATA");
             entEnveloppe.Property(e => e.EventType).IsRequired()
-                .ForSqliteHasColumnName("ETR_EVENT_TYPE")
-                .ForSqlServerHasColumnName("ETR_EVENT_TYPE");
+                .HasColumnName("ETR_EVENT_TYPE");
             entEnveloppe.Property(e => e.EventContextData).IsRequired(false)
-                .ForSqliteHasColumnName("ETR_EVENT_CONTEXT_DATA")
-                .ForSqlServerHasColumnName("ETR_EVENT_CONTEXT_DATA");
+                .HasColumnName("ETR_EVENT_CONTEXT_DATA");
             entEnveloppe.Property(e => e.ContextType).IsRequired(false)
-                .ForSqliteHasColumnName("ETR_EVENT_CONTEXT_TYPE")
-                .ForSqlServerHasColumnName("ETR_EVENT_CONTEXT_TYPE");
+                .HasColumnName("ETR_EVENT_CONTEXT_TYPE");
             entEnveloppe.Property(e => e.PeremptionDate).IsRequired()
-                .ForSqliteHasColumnName("ETR_PEREMPTIONDATE")
-                .ForSqlServerHasColumnName("ETR_PEREMPTIONDATE");
+                .HasColumnName("ETR_PEREMPTIONDATE");
 
             var entDispatched = modelBuilder.Entity<DispatchedEvent>();
             entDispatched.ToTable("EDP_T_EVENT_DISPATCHED");
             entDispatched.HasKey(e => e.Id);
             entDispatched.Property(e => e.EventId).IsRequired()
-                .ForSqliteHasColumnName("EDP_EVENT_ID")
-                .ForSqlServerHasColumnName("EDP_EVENT_ID");
+                .HasColumnName("EDP_EVENT_ID");
             entDispatched.Property(e => e.ReceiverId).IsRequired()
-                .ForSqliteHasColumnName("EDP_RECEIVER_ID")
-                .ForSqlServerHasColumnName("EDP_RECEIVER_ID");
+                .HasColumnName("EDP_RECEIVER_ID");
         }
 
         #endregion
