f24bbe80f17efb1bd1d0bcff49676b1399883d36
cdie <christophe.mommer@live.fr>
2017-08-25 21:47:09 +0200

    create test framework

---
+++ src/CQELight.TestFramework/BaseUnitTestClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using Xunit;
+
+namespace CQELight.TestFramework
+{
+    /// <summary>
+    /// Base class for unit testing classes.
+    /// </summary>
+    public class BaseUnitTestClass
+    {
+
+        #region Ctor
+
+        /// <summary>
+        /// Base constructeur for class.
+        /// </summary>
+        protected BaseUnitTestClass()
+        {
+            UnitTestTools.IsInUnitTestMode = true;
+            //To define integration mode, we use xUnit collection. This allow use also to disable parallelism for integration.
+            UnitTestTools.IsInIntegrationTestMode =
+                GetType().GetCustomAttribute<CollectionAttribute>() != null;
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/CQELight.TestFramework/UnitTestTools.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQELight.TestFramework
+{
+    /// <summary>
+    /// Bunch of tools only use in Unit test
+    /// </summary>
+    public static class UnitTestTools
+    {
+
+        /// <summary>
+        /// Flag to indicates if we're running in a unit test context.
+        /// </summary>
+        public static bool IsInUnitTestMode { get; internal set; } = false;
+        /// <summary>
+        /// Flag to indicates if we're running in integretation test context.
+        /// </summary>
+        public static bool IsInIntegrationTestMode { get; internal set; } = false;
+
+    }
+}
