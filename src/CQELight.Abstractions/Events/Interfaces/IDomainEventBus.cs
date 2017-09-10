83809f3ce4ed730ed688c2b29b21f706e13b77f2
cdie <christophe.mommer@live.fr>
2017-09-10 20:32:58 +0200

    missing cleaning file

--- src/CQELight.Abstractions/Events/Interfaces/IDomainEventBus.cs
+++ src/CQELight.Abstractions/Events/Interfaces/IDomainEventBus.cs
@@ -16,23 +16,5 @@ namespace CQELight.Abstractions.Events.Interfaces
         /// <param name="event">Event to register.</param>
         /// <param name="context">Context associated to the event..</param>
         Task RegisterAsync(IDomainEvent @event, IEventContext context = null);
-        /// <summary>
-        /// Register synchronously an event to be processed by the bus.
-        /// </summary>
-        /// <param name="event">Event to register.</param>
-        /// <param name="context">Context associated to the event..</param>
-        void Register(IDomainEvent @event, IEventContext context = null);
-        /// <summary>
-        /// Start the bus.
-        /// </summary>
-        void Start();
-        /// <summary>
-        /// Stop the bus.
-        /// </summary>
-        void Stop();
-        /// <summary>
-        /// Flag to indicates if bus is currently running.
-        /// </summary>
-        bool Working { get; }
     }
 }
