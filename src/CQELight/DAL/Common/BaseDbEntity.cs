467550c9de13291f4909b2a7949d5e339f2e30e5
cdie <christophe.mommer@gmail.com>
2018-03-06 11:32:37 +0100

    add base repositories objects

---
+++ src/CQELight/DAL/Common/BaseDbEntity.cs
@@ -0,0 +1,69 @@
+﻿using CQELight.DAL.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CQELight.DAL.Common
+{
+    /// <summary>
+    /// Base class definition for entity to be managed by database.
+    /// </summary>
+    public abstract class BaseDbEntity
+    {
+
+        #region CONSTS
+
+        /// <summary>
+        /// Name of column that holds deletion information.
+        /// </summary>
+        public const string CONST_DELETED_COLUMN = "DELETED";
+        /// <summary>
+        /// Name of column that holds deletion date.
+        /// </summary>
+        public const string CONST_DELETE_DATE_COLUMN = "DELETE_DATE";
+        /// <summary>
+        /// Name of column that holds edition date.
+        /// </summary>
+        public const string CONST_EDIT_DATE_COLUMN = "EDIT_DATE";
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Last edit date of the object.
+        /// </summary>
+        [Column(CONST_EDIT_DATE_COLUMN)]
+        public DateTime EditDate { get; set; }
+        /// <summary>
+        /// Soft deletion flag.
+        /// </summary>
+        [Column(CONST_DELETED_COLUMN), DefaultValue(false)]
+        public bool Deleted { get; set; }
+        /// <summary>
+        /// Soft deletion date.
+        /// </summary>
+        [Column(CONST_DELETE_DATE_COLUMN), DefaultValue(null)]
+        public DateTime? DeletionDate { get; set; }
+
+        #endregion
+
+        #region Abstract methods
+
+        /// <summary>
+        /// Check if key is defined or not. 
+        /// </summary>
+        /// <returns>True if key is defined, false otherwise.</returns>
+        public abstract bool IsKeySet();
+
+        /// <summary>
+        /// Get key value.
+        /// </summary>
+        /// <returns>Value of the key boxed in a object.</returns>
+        public abstract object GetKeyValue();
+
+        #endregion
+
+    }
+}
---
+++ src/CQELight/DAL/Common/ComposedKeyDbEntity.cs
@@ -0,0 +1,71 @@
+﻿using CQELight.DAL.Attributes;
+using CQELight.DAL.Exceptions;
+using CQELight.Tools.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CQELight.DAL.Common
+{
+    /// <summary>
+    /// Base class to manager composed key entities.
+    /// </summary>
+    public abstract class ComposedKeyDbEntity : BaseDbEntity
+    {
+
+        #region Overriden methods
+
+        public override object GetKeyValue()
+        {
+            var entityType = GetType();
+            var composedKeyAttr = entityType.GetCustomAttribute<ComposedKeyAttribute>();
+            if (composedKeyAttr != null)
+            {
+                var keyProps = entityType.GetAllProperties().Where(p => composedKeyAttr.PropertyNames.Contains(p.Name));
+                if (keyProps != null)
+                {
+                    var values = keyProps.Select(p => p.GetValue(this)).WhereNotNull();
+                    if (values != null && values.Any())
+                    {
+                        return string.Join(",", keyProps.Select(p => p.GetValue(this)));
+                    }
+                    return null;
+                }
+            }
+            throw new ComposedKeyAttributeNotDefinedException(entityType);
+        }
+
+        public override bool IsKeySet()
+        {
+            var entityType = GetType();
+            var composedKeyAttr = entityType.GetCustomAttribute<ComposedKeyAttribute>();
+            if (composedKeyAttr != null)
+            {
+                var keyProps = entityType.GetAllProperties().Where(p => composedKeyAttr.PropertyNames.Contains(p.Name));
+                if (keyProps != null)
+                {
+                    foreach (var key in keyProps)
+                    {
+                        object defaultValue = null;
+                        if (key.PropertyType.IsValueType)
+                        {
+                            defaultValue = key.PropertyType.CreateInstance();
+                        }
+                        if (key.GetValue(this) == defaultValue)
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+            throw new ComposedKeyAttributeNotDefinedException(entityType);
+        }
+
+        #endregion
+
+
+    }
+}
