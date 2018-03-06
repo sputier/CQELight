34936e54a94b1a0b9be72838c53ad334817d5a68
cdie <christophe.mommer@gmail.com>
2018-03-06 11:25:15 +0100

    create contract interface for repository

---
+++ src/CQELight/DAL/Interfaces/IDatabaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using CQELight.DAL.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQELight.DAL.Interfaces
+{
+    /// <summary>
+    /// Contract interface for repository on databases.
+    /// </summary>
+    /// <typeparam name="T">Type of entity to manage into database.</typeparam>
+    public interface IDatabaseRepository<T> : IDataReaderRepository<T>, IDataUpdateRepository<T>, IDisposable
+        where T : BaseDbEntity
+    {
+
+    }
+}
