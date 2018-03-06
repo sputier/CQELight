521192d3aadac8eb5d26cb286dbc43c29fece204
cdie <christophe.mommer@gmail.com>
2018-03-06 11:32:25 +0100

    add specific exceptions

---
+++ src/CQELight/DAL/Exceptions/ComposedKeyAttributeNotDefinedException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQELight.DAL.Exceptions
+{
+    /// <summary>
+    /// Specific exception to handle case when composed key attribute was not defined.
+    /// </summary>
+#pragma warning disable RCS1194, S3925
+    public class ComposedKeyAttributeNotDefinedException : Exception
+#pragma warning restore RCS1194, S3925
+    {
+
+        #region Properties
+        /// <summary>
+        /// Type of entity.
+        /// </summary>
+        public Type EntityType { get; private set; }
+
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        /// Creation of a new PrimaryKeyPropertyNotFound exception.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        public ComposedKeyAttributeNotDefinedException(Type entityType)
+            : base($"No composed key attribute has been define on type {entityType.FullName}.")
+        {
+            EntityType = entityType;
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/CQELight/DAL/Exceptions/PrimaryKeyPropertyNotFoundException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CQELight.DAL.Exceptions
+{
+#pragma warning disable RCS1194, S3925
+    /// <summary>
+    /// Specific exception to indicates that property that holds primary key hasn't been found.
+    /// </summary>
+    public class PrimaryKeyPropertyNotFoundException : Exception
+#pragma warning restore RCS1194, S3925
+    {
+
+        #region Properties
+        /// <summary>
+        /// Type of entity.
+        /// </summary>
+        public Type EntityType { get; private set; }
+
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        /// Creation of a new PrimaryKeyPropertyNotFound exception.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        public PrimaryKeyPropertyNotFoundException( Type entityType)
+            : base($"No primary key property hasn't been found on type {entityType.FullName}.")
+        {
+            EntityType = entityType;
+        }
+
+        #endregion
+
+    }
+}
