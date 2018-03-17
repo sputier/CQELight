a51b40df243d920893a17f7d469f91e80e1ac8ca
cdie <christophe.mommer@live.fr>
2018-03-17 18:56:49 +0100

    add in memory buses registration into bootrsapper

--- src/CQELight.Buses.InMemory/Bootstrapper.ext.cs
+++ src/CQELight.Buses.InMemory/Bootstrapper.ext.cs
@@ -1,4 +1,7 @@
-﻿using CQELight.Buses.InMemory.Events;
+﻿using CQELight.Abstractions.CQS.Interfaces;
+using CQELight.Abstractions.Events.Interfaces;
+using CQELight.Buses.InMemory.Commands;
+using CQELight.Buses.InMemory.Events;
 using CQELight.Dispatcher;
 using CQELight.IoC;
 using System;
@@ -18,6 +21,7 @@ namespace CQELight.Buses.InMemory
         public static Bootstrapper UseInMemoryEventBus(this Bootstrapper bootstrapper, InMemoryEventBusConfiguration configuration)
         {
             CoreDispatcher.ConfigureBus<InMemoryEventBus, InMemoryEventBusConfiguration>(configuration);
+            bootstrapper.AddIoCRegistration(new TypeRegistration(typeof(InMemoryEventBus), typeof(IDomainEventBus), typeof(InMemoryEventBus)));
             return bootstrapper;
         }
         /// <summary>
@@ -26,6 +30,7 @@ namespace CQELight.Buses.InMemory
         /// <param name="bootstrapper">Instance of boostrapper.</param>
         public static Bootstrapper UseInMemoryCommandBus(this Bootstrapper bootstrapper)
         {
+            bootstrapper.AddIoCRegistration(new TypeRegistration(typeof(InMemoryCommandBus), typeof(ICommandBus), typeof(InMemoryCommandBus)));
             return bootstrapper;
         }
 
