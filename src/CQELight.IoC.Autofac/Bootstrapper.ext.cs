b4f18a450e83586900e739352df9f5efcff318d2
cdie <christophe.mommer@live.fr>
2018-03-17 18:56:37 +0100

    update autofac factory to handle custom registrations

--- src/CQELight.IoC.Autofac/Bootstrapper.ext.cs
+++ src/CQELight.IoC.Autofac/Bootstrapper.ext.cs
@@ -1,12 +1,16 @@
 ﻿using Autofac;
+using CQELight.Abstractions.IoC.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CQELight.IoC.Autofac
 {
     public static class BootstrapperExt
     {
+
+        #region Public static methods
 
         /// <summary>
         /// Configure the bootstrapper to use Autofac as IoC.
@@ -21,10 +25,40 @@ namespace CQELight.IoC.Autofac
             }
 
             containerBuilder.RegisterModule<AutoRegisterModule>();
+            AddComponentRegistrationToContainer(containerBuilder, bootstrapper.IoCRegistrations);
+
+
             DIManager.Init(new AutofacScopeFactory(containerBuilder.Build()));
 
             return bootstrapper;
         }
+
+        #endregion
+
+        #region Private static methods
+
+        private static void AddComponentRegistrationToContainer(ContainerBuilder containerBuilder, IEnumerable<ITypeRegistration> customRegistration)
+        {
+            if(customRegistration?.Any() == false)
+            {
+                return;
+            }
+            foreach (var item in customRegistration)
+            {
+                if(item is InstanceTypeRegistration instanceTypeRegistration)
+                {
+                    containerBuilder.Register(c => instanceTypeRegistration.Value)
+                        .As(instanceTypeRegistration.Types.ToArray());
+                }
+                if(item is TypeRegistration typeRegistration)
+                {
+                    containerBuilder.RegisterType(typeRegistration.InstanceType)
+                        .As(typeRegistration.RegistrationTypes.ToArray());
+                }
+            }
+        }
+
+        #endregion
 
     }
 }
