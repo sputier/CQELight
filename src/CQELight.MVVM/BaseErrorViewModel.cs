1ab8e3d3a351d95476f977d9d6141f7992023456
cdie <christophe.mommer@live.fr>
2018-03-18 09:03:03 +0100

    create baseViewModels

---
+++ src/CQELight.MVVM/BaseErrorViewModel.cs
@@ -0,0 +1,110 @@
+﻿using CQELight.MVVM.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CQELight.MVVM
+{
+    /// <summary>
+    /// Base class for view model that can handle errors.
+    /// </summary>
+    public abstract class BaseErrorViewModel : BaseViewModel, INotifyDataErrorInfo
+    {
+
+        #region Members
+
+        private readonly IDictionary<string, IList<string>> _errors 
+            = new Dictionary<string, IList<string>>();
+
+        #endregion
+
+        #region Ctor
+
+        protected BaseErrorViewModel()
+            : base()
+        {
+        }
+
+        protected BaseErrorViewModel(IView holder)
+            : base(holder)
+        {
+        }
+
+        #endregion
+
+        #region INotifyDataErrorInfo
+
+        /// <summary>
+        /// Gets the info if there's any informations.
+        /// </summary>
+        public bool HasErrors => _errors.Any();
+        /// <summary>
+        /// Event to fire when error collection are changed.
+        /// </summary>
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        /// <summary>
+        /// Get errors based on property names.
+        /// </summary>
+        /// <param name="propertyName">Property name on which we wnat errors..</param>
+        /// <returns>All errors for this property name, or empty collection, or null if none.</returns>
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (!string.IsNullOrWhiteSpace(propertyName))
+            {
+                return _errors.ContainsKey(propertyName) ? _errors[propertyName] : null;
+            }
+            return null;
+        }
+
+        #endregion
+
+        #region Protected methods
+
+        /// <summary>
+        /// Add an error in the collection.
+        /// </summary>
+        /// <param name="propertyName">Name of the property. Can be retrieve by the calling member.</param>
+        /// <param name="error">Error info to add to collection.</param>
+        protected void AddError(string error, [CallerMemberName]string propertyName = "")
+        {
+            if (!_errors.ContainsKey(propertyName))
+            {
+                _errors[propertyName] = new List<string>();
+            }
+            if (!_errors[propertyName].Contains(error))
+            {
+                _errors[propertyName].Add(error);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        /// <summary>
+        /// Clear all errors for a specific property name.
+        /// </summary>
+        /// <param name="propertyName">Name of the property.</param>
+        protected void ClearErrors([CallerMemberName]string propertyName = "")
+        {
+            if (_errors.ContainsKey(propertyName))
+            {
+                _errors.Remove(propertyName);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        /// <summary>
+        /// Method to notify listeners that error are changed.
+        /// </summary>
+        /// <param name="propertyName">Name of the property.</param>
+        protected virtual void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+            RaisePropertyChanged(nameof(HasErrors));
+        }
+
+        #endregion
+    }
+}
---
+++ src/CQELight.MVVM/BaseViewModel.cs
@@ -0,0 +1,111 @@
+﻿using CQELight.Abstractions.CQS.Interfaces;
+using CQELight.Abstractions.Events.Interfaces;
+using CQELight.Abstractions.IoC.Interfaces;
+using CQELight.Dispatcher;
+using CQELight.IoC;
+using CQELight.MVVM.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using W = System.Windows.Input;
+
+namespace CQELight.MVVM
+{
+    /// <summary>
+    /// Definition class for view models. A view model is an observable object that can notify view for changes.
+    /// It's also a IoC scope holder, a command context for dispatching, an event context for dispatching and 
+    /// is disposable.
+    /// </summary>
+    public abstract class BaseViewModel : ObservableObject, IScopeHolder, ICommandContext, IEventContext, IDisposable
+    {
+
+        #region Members
+
+        protected IScope _scope;
+        protected IView _view;
+        protected ILogger _logger;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// IoC Scope.
+        /// </summary>
+        public IScope Scope => _scope;
+
+        /// <summary>
+        /// Title of window.
+        /// </summary>
+        public string Title
+        {
+            get => _title;
+            protected set => Set(ref _title, value);
+        }
+        private string _title;
+
+        /// <summary>
+        /// Command for cancel action.
+        /// </summary>
+        public W.ICommand CancelCommand
+            => new DelegateCommand(_ => Cancel());
+
+        #endregion
+
+        #region Ctor
+
+
+        protected BaseViewModel()
+        {
+            _scope = DIManager.BeginScope();
+            _logger = _scope.Resolve<ILoggerFactory>()?.CreateLogger(GetType().Name);
+            CoreDispatcher.AddHandlerToDispatcher(this);
+        }
+
+        protected BaseViewModel(IView view)
+            : this()
+        {
+            _view = view;
+        }
+
+        #endregion
+
+        #region public methods
+
+        /// <summary>
+        /// Base method to cancel action.
+        /// </summary>
+        public virtual void Cancel()
+            => _view.Close();
+
+        /// <summary>
+        /// Action to perform when view is loaded.
+        /// </summary>
+        public virtual Task OnLoadCompleteAsync()
+        {
+            return Task.FromResult(0);
+        }
+
+        /// <summary>
+        /// Action to perform when view is closing.
+        /// </summary>
+        /// <returns>If true is returned, view can closed. If false is returned, closing is canceled.</returns>
+        public virtual Task<bool> OnCloseAsync()
+        {
+            return Task.FromResult(true);
+        }
+
+        /// <summary>
+        /// Cleaning up viewmodel resources.
+        /// </summary>
+        public virtual void Dispose()
+        {
+
+        }
+
+        #endregion
+
+    }
+}
