ea1c37edbc591a32b2990f70a57108a83a9b4945
cdie <christophe.mommer@gmail.com>
2018-03-26 09:57:42 +0200

    add some get tests

--- tests/CQELight.DAL.EFCore.Integration.Tests/EFRepository.Tests.cs
+++ tests/CQELight.DAL.EFCore.Integration.Tests/EFRepository.Tests.cs
@@ -13,13 +13,13 @@ namespace CQELight.DAL.EFCore.Integration.Tests
 
     #region Nested class
 
-    class TestBlogEFRepository : EFRepository<Blog>
+    class TestBlogEFRepository : EFRepository<WebSite>
     {
         public TestBlogEFRepository() : base(new TestDbContext())
         {
         }
 
-        public Blog GetWithPostAndComment()
+        public WebSite GetWithPostAndComment()
             => DataSet
                 .Include(b => b.Posts)
                 .ThenInclude(p => p.Writer)
@@ -47,6 +47,252 @@ namespace CQELight.DAL.EFCore.Integration.Tests
                 }
                 _isInit = true;
             }
+            DeleteAll();
+        }
+
+        private void DeleteAll()
+        {
+            using (var ctx = new TestDbContext())
+            {
+                ctx.RemoveRange(ctx.Set<Hyperlink>());
+                ctx.RemoveRange(ctx.Set<WebSite>());
+                ctx.SaveChanges();
+            }
+        }
+
+        #endregion
+
+        #region Get
+
+        [Fact]
+        public async Task EFRepository_SimpleGet_NoFilter_NoOrder_NoIncludes_Should_Returns_All()
+        {
+            try
+            {
+                using (var ctx = new TestDbContext())
+                {
+                    ctx.Add(new WebSite
+                    {
+                        Url = "https://blogs.msdn.net"
+                    });
+                    ctx.Add(new WebSite
+                    {
+                        Url = "https://www.microsoft.com"
+                    });
+                    await ctx.SaveChangesAsync();
+                }
+
+                using (var repo = new TestBlogEFRepository())
+                {
+                    var sites = await repo.GetAsync().ToList();
+                    sites.Should().HaveCount(2);
+                    sites.Any(s => s.Url.Contains("msdn")).Should().BeTrue();
+                    sites.Any(s => s.Url.Contains("microsoft")).Should().BeTrue();
+                }
+            }
+            finally
+            {
+                DeleteAll();
+            }
+        }
+
+        [Fact]
+        public async Task EFRepository_SimpleGet_WithFilter_NoOrder_NoIncludes_Should_Returns_FilteredItemsOnly()
+        {
+            try
+            {
+                using (var ctx = new TestDbContext())
+                {
+                    ctx.Add(new WebSite
+                    {
+                        Url = "https://blogs.msdn.net"
+                    });
+                    ctx.Add(new WebSite
+                    {
+                        Url = "https://www.microsoft.com"
+                    });
+                    await ctx.SaveChangesAsync();
+                }
+
+                using (var repo = new TestBlogEFRepository())
+                {
+                    var sites = await repo.GetAsync(w => w.Url.Contains("msdn")).ToList();
+                    sites.Should().HaveCount(1);
+                    sites.Any(s => s.Url.Contains("msdn")).Should().BeTrue();
+                    sites.Any(s => s.Url.Contains("microsoft")).Should().BeFalse();
+                }
+            }
+            finally
+            {
+                DeleteAll();
+            }
+        }
+
+
+        [Fact]
+        public async Task EFRepository_SimpleGet_NoFilter_NohOrder_NoIncludes_WithDeleted_Should_GetAll()
+        {
+            try
+            {
+                using (var ctx = new TestDbContext())
+                {
+                    ctx.Add(new WebSite
+                    {
+                        Url = "https://blogs.msdn.net"
+                    });
+                    ctx.Add(new WebSite
+                    {
+                        Url = "https://www.microsoft.com",
+                        Deleted = true,
+                        DeletionDate = DateTime.Now
+                    });
+                    await ctx.SaveChangesAsync();
+                }
+
+                using (var repo = new TestBlogEFRepository())
+                {
+                    var sites = await repo.GetAsync(includeDeleted: true).ToList();
+                    sites.Should().HaveCount(2);
+                    sites.Any(s => s.Url.Contains("msdn")).Should().BeTrue();
+                    sites.Any(s => s.Url.Contains("microsoft")).Should().BeTrue();
+                    sites.Any(s => s.Deleted).Should().BeTrue();
+                    sites.Any(s => !s.Deleted).Should().BeTrue();
+                }
+            }
+            finally
+            {
+                DeleteAll();
+            }
+        }
+
+        [Fact]
+        public async Task EFRepository_SimpleGet_NoFilter_WithOrder_NoIncludes_Should_Respect_Order()
+        {
+            try
+            {
+                using (var ctx = new TestDbContext())
+                {
+                    ctx.Add(new WebSite
+                    {
+                        Url = "https://blogs.msdn.net"
+                    });
+                    ctx.Add(new WebSite
+                    {
+                        Url = "https://www.microsoft.com"
+                    });
+                    await ctx.SaveChangesAsync();
+                }
+
+                using (var repo = new TestBlogEFRepository())
+                {
+                    var sites = await repo.GetAsync(orderBy: b => b.Url).ToList();
+                    sites.Should().HaveCount(2);
+                    sites.Any(s => s.Url.Contains("msdn")).Should().BeTrue();
+                    sites.Any(s => s.Url.Contains("microsoft")).Should().BeTrue();
+                    sites.First().Url.Should().Contain("msdn");
+                }
+            }
+            finally
+            {
+                DeleteAll();
+            }
+        }
+
+        [Fact]
+        public async Task EFRepository_SimpleGet_NoFilter_NoOrder_WithIncludes_Should_GetLinkedData()
+        {
+            try
+            {
+                using (var ctx = new TestDbContext())
+                {
+                    ctx.Add(new WebSite
+                    {
+                        Url = "https://blogs.msdn.net",
+                        HyperLinks = new List<Hyperlink>
+                        {
+                            new Hyperlink
+                            {
+                                Value = "https://blogs.msdn.net"
+                            },
+                            new Hyperlink
+                            {
+                                Value = "https://blogs2.msdn.net"
+                            }
+                        }
+                    });
+                    ctx.Add(new WebSite
+                    {
+                        Url = "https://www.microsoft.com",
+                        HyperLinks = new List<Hyperlink>
+                        {
+                            new Hyperlink
+                            {
+                                Value = "https://www.microsoft.com"
+                            },
+                            new Hyperlink
+                            {
+                                Value = "https://www.visualstudio.com"
+                            }
+                        }
+                    });
+                    await ctx.SaveChangesAsync();
+                }
+
+                using (var repo = new TestBlogEFRepository())
+                {
+                    var sites = await repo.GetAsync(includes: w => w.HyperLinks).ToList();
+                    sites.Should().HaveCount(2);
+                    sites.Any(s => s.Url.Contains("msdn")).Should().BeTrue();
+                    sites.Any(s => s.Url.Contains("microsoft")).Should().BeTrue();
+
+                    var site = sites.FirstOrDefault(s => s.Url.Contains("msdn"));
+                    site.HyperLinks.Should().HaveCount(2);
+                    site.HyperLinks.Any(u => u.Value.Contains("blogs.")).Should().BeTrue();
+                    site.HyperLinks.Any(u => u.Value.Contains("blogs2.")).Should().BeTrue();
+                }
+            }
+            finally
+            {
+                DeleteAll();
+            }
+        }
+
+        #endregion
+
+        #region GetByIdAsync
+
+        [Fact]
+        public async Task EFRepository_GetByIdAsync_AsExpected()
+        {
+            try
+            {
+                Guid? id = null;
+                using (var ctx = new TestDbContext())
+                {
+                    var w = new WebSite
+                    {
+                        Url = "https://blogs.msdn.net"
+                    };
+                    ctx.Add(w);
+                    ctx.Add(new WebSite
+                    {
+                        Url = "https://www.microsoft.com"
+                    });
+                    await ctx.SaveChangesAsync();
+                    id = w.Id;
+                }
+
+                using (var repo = new TestBlogEFRepository())
+                {
+                    var result = await repo.GetByIdAsync(id);
+                    result.Should().NotBeNull();
+                    result.Url.Should().Be("https://blogs.msdn.net");
+                }
+            }
+            finally
+            {
+                DeleteAll();
+            }
         }
 
         #endregion
@@ -56,64 +302,78 @@ namespace CQELight.DAL.EFCore.Integration.Tests
         [Fact]
         public async Task EFRepository_Physical_Deletion()
         {
-            using (var repo = new TestBlogEFRepository())
+            try
             {
-                var entity = new Blog
+                using (var ctx = new TestDbContext())
                 {
-                    Url = "http://dotnet.microsoft.com/"
-                };
-                repo.MarkForInsert(entity);
-                await repo.SaveAsync();
+                    var entity = new WebSite
+                    {
+                        Url = "http://dotnet.microsoft.com/"
+                    };
+                    ctx.Add(entity);
+                    await ctx.SaveChangesAsync();
+                }
+                using (var repo = new TestBlogEFRepository())
+                {
+                    repo.Get().Count().Should().Be(1);
+                    repo.Get(includeDeleted: true).Count().Should().Be(1);
+                }
+                using (var repo = new TestBlogEFRepository())
+                {
+                    var entity = repo.Get().FirstOrDefault();
+                    entity.Should().NotBeNull();
+                    repo.MarkForDelete(entity, true);
+                    await repo.SaveAsync();
+                }
+                using (var repo = new TestBlogEFRepository())
+                {
+                    repo.Get(includeDeleted: true).Count().Should().Be(0);
+                    repo.Get().Count().Should().Be(0);
+                }
             }
-            using (var repo = new TestBlogEFRepository())
+            finally
             {
-                repo.Get().Count().Should().Be(1);
-                repo.Get(includeDeleted: true).Count().Should().Be(1);
-            }
-            using (var repo = new TestBlogEFRepository())
-            {
-                var entity = repo.Get().FirstOrDefault();
-                entity.Should().NotBeNull();
-                repo.MarkForDelete(entity, true);
-                await repo.SaveAsync();
-            }
-            using (var repo = new TestBlogEFRepository())
-            {
-                repo.Get(includeDeleted: true).Count().Should().Be(0);
-                repo.Get().Count().Should().Be(0);
+                DeleteAll();
             }
         }
 
         [Fact]
         public async Task EFRepository_Logical_Deletion()
         {
-            using (var repo = new TestBlogEFRepository())
+            try
             {
-                var entity = new Blog
+                using (var ctx = new TestDbContext())
                 {
-                    Url = "http://dotnet.microsoft.com/"
-                };
-                repo.MarkForInsert(entity);
-                await repo.SaveAsync();
+                    var entity = new WebSite
+                    {
+                        Url = "http://dotnet.microsoft.com/"
+                    };
+                    ctx.Add(entity);
+                    await ctx.SaveChangesAsync();
+                }
+                using (var repo = new TestBlogEFRepository())
+                {
+                    repo.Get().Count().Should().Be(1);
+                    repo.Get(includeDeleted: true).Count().Should().Be(1);
+                }
+                using (var repo = new TestBlogEFRepository())
+                {
+                    var entity = repo.Get().FirstOrDefault();
+                    entity.Should().NotBeNull();
+                    repo.MarkForDelete(entity);
+                    await repo.SaveAsync();
+                }
+                using (var repo = new TestBlogEFRepository())
+                {
+                    repo.Get(includeDeleted: true).Count().Should().Be(1);
+                    var entity = repo.Get(includeDeleted: true).FirstOrDefault();
+                    entity.DeletionDate.Should().BeSameDateAs(DateTime.Today);
+                    repo.Get().Count().Should().Be(0);
+                }
             }
-            using (var repo = new TestBlogEFRepository())
+            finally
             {
-                repo.Get().Count().Should().Be(1);
-                repo.Get(includeDeleted: true).Count().Should().Be(1);
-            }
-            using (var repo = new TestBlogEFRepository())
-            {
-                var entity = repo.Get().FirstOrDefault();
-                entity.Should().NotBeNull();
-                repo.MarkForDelete(entity);
-                await repo.SaveAsync();
-            }
-            using (var repo = new TestBlogEFRepository())
-            {
-                repo.Get(includeDeleted: true).Count().Should().Be(1);
-                var entity = repo.Get(includeDeleted: true).FirstOrDefault();
-                entity.DeletionDate.Should().BeSameDateAs(DateTime.Today);
-                repo.Get().Count().Should().Be(0);
+                DeleteAll();
             }
         }
 
