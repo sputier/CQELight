ab77320b07434d342c24a9fb14d9449a3ee823cc
cdie <christophe.mommer@gmail.com>
2018-03-27 11:39:56 +0200

    provide implementation for generics messages

---
+++ src/CQELight/Abstractions/Dispatcher/IMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQELight.Abstractions.Dispatcher
+{
+    /// <summary>
+    /// Base contract interface for standard message in system.
+    /// </summary>
+    public interface IMessage
+    {
+    }
+}
