755eac1cc56ccb494b4eb963931c5fd4e6108867
cdie <christophe.mommer@gmail.com>
2018-04-03 14:38:18 +0200

    create base structure for transactionnal events

---
+++ src/CQELight/Abstractions/Events/Interfaces/ITransactionnalEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQELight.Abstractions.Events.Interfaces
+{
+    /// <summary>
+    /// Contract interface for handler that need to handle a transactionnal event.
+    /// </summary>
+    /// <typeparam name="T">Type of transactionnal event to handle.</typeparam>
+    public interface ITransactionnalEventHandler<T>
+        where T : ITransactionnalEvent
+    {
+
+        /// <summary>
+        /// Handle asynchronously a transactionnal event.
+        /// </summary>
+        /// <param name="transactionnalEvent">Transactionnal event instance.</param>
+        /// <param name="context">Dispatching context.</param>
+        Task HandleAsync(T transactionnalEvent, IEventContext context = null);
+
+    }
+}
