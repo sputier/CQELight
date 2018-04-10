db145da47de4a7b4b5242c4dacb150fb5488db76
cdie <christophe.mommer@gmail.com>
2018-04-10 14:44:20 +0200

    last update according to release

--- examples/desktop/CQELight.Examples.Console/AppDbContext.cs
+++ examples/desktop/CQELight.Examples.Console/AppDbContext.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace CQELight.Examples.Console
 {
-    class Consts
+    static class Consts
     {
         public const string CONST_CONNECTION_STRING = "Server=(localdb)\\mssqllocaldb;Database=TestApp_Base;Trusted_Connection=True;MultipleActiveResultSets=true;";
         public const string CONST_EVENT_DB_CONNECTION_STRING = "Server=(localdb)\\mssqllocaldb;Database=TestApp_Events_Base;Trusted_Connection=True;MultipleActiveResultSets=true;";
--- src/CQELight/Dispatcher/Serializers/JsonDispatcherSerializer.cs
+++ src/CQELight/Dispatcher/Serializers/JsonDispatcherSerializer.cs
@@ -16,6 +16,11 @@ namespace CQELight.Events.Serializers
 
         #region IDispatcherSerializer
 
+        /// <summary>
+        /// Deserialize a command from Json data.
+        /// </summary>
+        /// <param name="data">Json data.</param>
+        /// <returns>Instance of deserialized command.</returns>
         public ICommand DeserializeCommand(string data)
         {
             if (string.IsNullOrWhiteSpace(data))
@@ -25,6 +30,12 @@ namespace CQELight.Events.Serializers
             return Newtonsoft.Json.JsonConvert.DeserializeObject<ICommand>(data);
         }
 
+        /// <summary>
+        /// Deserialize a command from Json data.
+        /// </summary>
+        /// <typeparam name="T">Type of command to retrieve.</typeparam>
+        /// <param name="data">Json data.</param>
+        /// <returns>Instance of deserialized command.</returns>
         public T DeserializeCommand<T>(string data) where T : ICommand
         {
             if (string.IsNullOrWhiteSpace(data))
@@ -34,6 +45,11 @@ namespace CQELight.Events.Serializers
             return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(data);
         }
 
+        /// <summary>
+        /// Deserialize an event from Json data.
+        /// </summary>
+        /// <param name="data">Json data.</param>
+        /// <returns>Instance of deserialized event.</returns>
         public IDomainEvent DeserializeEvent(string data)
         {
             if (string.IsNullOrWhiteSpace(data))
@@ -43,6 +59,12 @@ namespace CQELight.Events.Serializers
             return Newtonsoft.Json.JsonConvert.DeserializeObject<IDomainEvent>(data);
         }
 
+        /// <summary>
+        /// Deserialize an event from Json data.
+        /// </summary>
+        /// <typeparam name="T">Type of event to retrieve.</typeparam>
+        /// <param name="data">Json data.</param>
+        /// <returns>Instance of deserialized event.</returns>
         public T DeserializeEvent<T>(string data) where T : IDomainEvent
         {
             if (string.IsNullOrWhiteSpace(data))
@@ -52,6 +74,11 @@ namespace CQELight.Events.Serializers
             return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(data);
         }
 
+        /// <summary>
+        /// Serialize a command to a Json string.
+        /// </summary>
+        /// <param name="command">Command to serialize.</param>
+        /// <returns>Command serialized as string.</returns>
         public string SerializeCommand(ICommand command)
         {
             if (command == null)
@@ -61,6 +88,11 @@ namespace CQELight.Events.Serializers
             return Newtonsoft.Json.JsonConvert.SerializeObject(command);
         }
 
+        /// <summary>
+        /// Serialize an event to a Json string.
+        /// </summary>
+        /// <param name="event">Event to serialize.</param>
+        /// <returns>Event serialized as string.</returns>
         public string SerializeEvent(IDomainEvent @event)
         {
             if (@event == null)
