8da2ebb61276bd9ac52c06a1eff60ae865dfa992
CHRISTOPHE\Christophe <christophe.m@jvweb.fr>
2018-06-01 15:17:34 +0200

    creation for base for azure service bus

---
+++ src/CQELight.Buses.AzureServiceBus/Bootstrapper.ext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CQELight.Buses.AzureServiceBus
+{
+    public static class BootstrapperExt
+    {
+
+        #region Public static methods
+
+        public static Bootstrapper UseAzureServiceBus(this Bootstrapper bootstrapper, string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentException("Bootstrapper.UseAzureServiceBus() : Connection string should be provided.", nameof(connectionString));
+            }
+
+            return bootstrapper;
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/CQELight.Buses.AzureServiceBus/Client/AzureServiceBusClient.cs
@@ -0,0 +1,24 @@
+﻿using CQELight.Abstractions.Events.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQELight.Buses.AzureServiceBus.Client
+{
+
+    class AzureServiceBusClient : IDomainEventBus
+    {
+
+        #region IDomainEventBus methods
+
+
+        public Task RegisterAsync(IDomainEvent @event, IEventContext context = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
