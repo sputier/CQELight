3dec53c50283bbf02a9fc436fb1321a8656df99d
cdie <christophe.mommer@live.fr>
2018-06-21 07:28:51 +0200

    move to samples folder and create hello world event

---
+++ samples/documentation/1.HelloWorld/HelloWorld/Events/GreetingsEvent.cs
@@ -0,0 +1,11 @@
+﻿using CQELight.Abstractions.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Events
+{
+    class GreetingsEvent : BaseDomainEvent
+    {
+    }
+}
