36e4e59aacc25055532b9cdcd96940380655dce7
cdie <christophe.mommer@live.fr>
2018-06-21 07:28:51 +0200

    move to samples folder and create hello world event

---
+++ samples/documentation/1.HelloWorld/HelloWorld/Handlers/GreetingsEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using CQELight.Abstractions.Events.Interfaces;
+using HelloWorld.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Handlers
+{
+    class GreetingsEventHandler : IDomainEventHandler<GreetingsEvent>
+    {
+        public Task HandleAsync(GreetingsEvent domainEvent, IEventContext context = null)
+        {
+            Console.WriteLine("Hello world!");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ samples/documentation/1.HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using CQELight;
+using CQELight.Dispatcher;
+using HelloWorld.Events;
+using System;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            new Bootstrapper().
+                UseInMemoryEventBus()
+                .Bootstrapp();
+
+            await CoreDispatcher.PublishEventAsync(new GreetingsEvent()).ConfigureAwait(false);
+
+            Console.Read();
+        }
+    }
+}
