1705038b8917c901a93ca90042a224ed410e3103
Christophe MOMMER <christophe.mommer-lememo@outlook.fr>
2018-07-19 13:05:19 +0200

    adding command + handler

---
+++ samples/documentation/2.Geneao/Geneao/Commands/AjouterPersonneCommand.cs
@@ -0,0 +1,32 @@
+﻿using CQELight.Abstractions.CQS.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geneao.Commands
+{
+    public sealed class AjouterPersonneCommand : ICommand
+    {
+        public string Nom { get; private set; }
+        public string Prenom { get; private set; }
+        public string LieuNaissance { get; private set; }
+        public DateTime DateNaissance { get; private set; }
+
+        private AjouterPersonneCommand() { }
+
+        public AjouterPersonneCommand(string nom, string prenom, string lieuNaissance, DateTime dateNaissance)
+        {
+            if (string.IsNullOrWhiteSpace(nom)) throw new ArgumentException("AjouterPersonneCommand.Ctor() : Nom requis.", nameof(nom));
+
+            if (string.IsNullOrWhiteSpace(prenom)) throw new ArgumentException("AjouterPersonneCommand.Ctor() : Prénom requis.", nameof(prenom));
+
+            if (string.IsNullOrWhiteSpace(lieuNaissance)) throw new ArgumentException("AjouterPersonneCommand.Ctor() : Lieu naissance requis.", nameof(lieuNaissance));
+
+            DateNaissance = dateNaissance;
+            LieuNaissance = lieuNaissance;
+            Prenom = prenom;
+            Nom = nom;
+        }
+    }
+
+}
---
+++ samples/documentation/2.Geneao/Geneao/Handlers/Commands/AjouterPersonneCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using CQELight.Abstractions.CQS.Interfaces;
+using Geneao.Commands;
+using Geneao.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geneao.Handlers.Commands
+{
+    class AjouterPersonneCommandHandler : ICommandHandler<AjouterPersonneCommand>
+    {
+        public Task HandleAsync(AjouterPersonneCommand command, ICommandContext context = null)
+        {
+            var famille = new Famille();
+            famille.AjouterPersonne(command.Nom, command.Prenom, new InfosNaissance(command.LieuNaissance, command.DateNaissance));
+            return Task.CompletedTask;
+        }
+    }
+
+}
