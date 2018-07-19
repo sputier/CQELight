de204569786ba510d7b10159c6873218c15b59f8
Christophe MOMMER <christophe.mommer-lememo@outlook.fr>
2018-07-19 13:01:50 +0200

    creation of geneao sample with domain objects

---
+++ samples/documentation/2.Geneao/Geneao/Domain/Famille.cs
@@ -0,0 +1,42 @@
+﻿using CQELight.Abstractions.DDD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geneao.Domain
+{
+    class Famille : AggregateRoot<Guid>
+    {
+
+        public IEnumerable<Personne> Personnes => _state.Personnes.AsEnumerable();
+
+        private FamilleState _state;
+
+        private class FamilleState : AggregateState
+        {
+
+            public List<Personne> Personnes { get; set; }
+
+            public FamilleState()
+            {
+                Personnes = new List<Personne>();
+            }
+        }
+
+        public Famille()
+        {
+            _state = new FamilleState();
+        }
+
+        public void AjouterPersonne(string nom, string prenom, InfosNaissance infosNaissance)
+        {
+            if (!_state.Personnes.Any(p => p.Nom == nom && p.Prenom == prenom && p.InfosNaissance == infosNaissance))
+            {
+                _state.Personnes.Add(Personne.DeclarerNaissance(nom, prenom, infosNaissance));
+            }
+        }
+
+    }
+
+}
---
+++ samples/documentation/2.Geneao/Geneao/Domain/Personne.cs
@@ -0,0 +1,51 @@
+﻿using CQELight.Abstractions.DDD;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geneao.Domain
+{
+    class Personne : Entity<Guid>
+    {
+
+        #region Properties
+
+        public string Nom { get; private set; }
+        public string Prenom { get; private set; }
+        public InfosNaissance InfosNaissance { get; private set; }
+
+        #endregion
+
+        #region Ctor
+
+        private Personne()
+        {
+
+        }
+
+        #endregion
+
+        #region Public static methods
+
+        public static Personne DeclarerNaissance(string nom, string prenom, InfosNaissance infosNaissance)
+        {
+            if (string.IsNullOrWhiteSpace(nom)) throw new ArgumentException("Personne.DeclarerNaissance() : Nom requis", nameof(nom));
+
+            if (string.IsNullOrWhiteSpace(prenom)) throw new ArgumentException("Personne.DeclarerNaissance() : Prénom requis", nameof(prenom));
+
+            if (infosNaissance == null) throw new ArgumentNullException(nameof(infosNaissance));
+
+            return new Personne
+            {
+                Id = Guid.NewGuid(),
+                Nom = nom,
+                Prenom = prenom,
+                InfosNaissance = infosNaissance
+            };
+        }
+
+        #endregion
+
+    }
+
+}
