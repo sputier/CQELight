726c069e343416e34510cdda789fc80a81d5420b
Christophe MOMMER <christophe.mommer-lememo@outlook.fr>
2018-08-08 13:24:31 +0200

    project creation + ensure compatibility with full framework for msmq

---
+++ src/CQELight.Buses.MSMQ/Bootstrapper.ext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQELight.Buses.MSMQ
+{
+    public static class BootstrapperExtensions
+    {
+        #region Public static methods
+
+        /// <summary>
+        /// Use MSMQ as bus for messaging.
+        /// </summary>
+        /// <param name="bootstrapper">Bootstrapper instance.</param>
+        /// <returns>Bootstrapper instance.</returns>
+        public static Bootstrapper UseMSMQClientBus(this Bootstrapper bootstrapper)
+        {
+            var service = MSMQBootstrappService.Instance;
+
+            service.BootstrappAction = () =>
+             {
+
+             };
+
+            if (!bootstrapper.RegisteredServices.Any(s => s == service))
+            {
+                bootstrapper.AddService(service);
+            }
+
+            return bootstrapper;
+        }
+
+        #endregion
+
+    }
+}
