793790b7fd0e78897092a6d1f799570bbff59af6
Christophe MOMMER <christophe.mommer-lememo@outlook.fr>
2018-09-18 12:06:45 +0200

    adding cosmosdb benchmarks + handling sqlite/sqlserver benchmark

--- src/CQELight.EventStore.CosmosDb/InternalsVisibleTo.cs
+++ src/CQELight.EventStore.CosmosDb/InternalsVisibleTo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("CQELight.EventStore.CosmosDb.Integration.Tests")]
-[assembly:InternalsVisibleTo("CQELight_EventStore_CosmosDb_Benchmarks")]
+[assembly:InternalsVisibleTo("CQELight_Benchmarks")]
