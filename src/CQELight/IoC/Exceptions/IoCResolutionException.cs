e89c8d3c95dc71bd1e02d9d2ae6b9656d908ede8
cdie <christophe.mommer@gmail.com>
2018-11-22 08:46:57 +0100

    prevents multiple failing resolution of event handler from scope

---
+++ src/CQELight/IoC/Exceptions/IoCResolutionException.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQELight.IoC.Exceptions
+{
+    /// <summary>
+    /// A generic exception that indicates an IoC resolution exception. 
+    /// </summary>
+    public class IoCResolutionException : System.Exception
+    {
+     
+        #region Ctor
+
+        /// <summary>
+        /// Creates a new empty IoCResolutionException.
+        /// </summary>
+        public IoCResolutionException() 
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Creates a new IoCResolutionException with a specific message.
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public IoCResolutionException(string message) 
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new IoCResolutionException with a specific message 
+        /// and the IoC provider specific exception
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="innerException">IoC provider specific exception.</param>
+        public IoCResolutionException(string message, Exception innerException) 
+            : base(message, innerException)
+        {
+
+        }
+
+        #endregion
+
+    }
+}
