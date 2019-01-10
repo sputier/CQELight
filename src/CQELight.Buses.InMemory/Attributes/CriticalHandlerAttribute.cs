3c7e432da99de455bd7bd4e93ef1d0e83772fe17
cdie <christophe.mommer@gmail.com>
2019-01-10 12:08:19 +0100

    comment typo

--- src/CQELight.Buses.InMemory/Attributes/CriticalHandlerAttribute.cs
+++ src/CQELight.Buses.InMemory/Attributes/CriticalHandlerAttribute.cs
@@ -6,7 +6,7 @@ namespace CQELight.Buses.InMemory
 {
     /// <summary>
     /// Attribute for defining that a specific handler is critical,
-    /// meaning that if this handler failed, the next ones should be invoked.
+    /// meaning that if this handler failed, the next ones shouldn't be invoked.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class CriticalHandlerAttribute : Attribute
