63f2d27d982c2ad6cc87dd68eba0ee539999c9aa
cdie <christophe.mommer@gmail.com>
2019-01-18 14:16:46 +0100

    add implementation of microsoft.extensions.dependencyinjections

---
+++ src/CQELight.IoC.Microsoft.Extensions.DependencyInjection/MicrosoftDependencyInjectionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQELight.IoC.Microsoft.Extensions.DependencyInjection
+{
+    class MicrosoftDependencyInjectionService : IBootstrapperService
+    {
+        public BootstrapperServiceType ServiceType => BootstrapperServiceType.IoC;
+        public Action<BootstrappingContext> BootstrappAction { get; internal set; }
+    }
+}
