b8542e631459d65825ddcc20df38fc5c46e1f220
cdie <christophe.mommer@live.fr>
2019-02-03 10:24:35 +0100

    creation of generic type parameter + perf enhancements

--- src/CQELight/IoC/TypeRegistration`.cs
+++ src/CQELight/IoC/TypeRegistration`.cs
@@ -10,28 +10,9 @@ namespace CQELight.IoC
     /// Generic type registration.
     /// </summary>
     /// <typeparam name="T">Type of class to register.</typeparam>
-    public class TypeRegistration<T> : ITypeRegistration
+    public class TypeRegistration<T> : TypeRegistration
         where T : class
     {
-        #region Members
-
-        private readonly List<Type> _abstractionTypes = new List<Type>();
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Type registrations.
-        /// </summary>
-        public IEnumerable<Type> AbstractionTypes { get => _abstractionTypes.AsEnumerable(); }
-
-        /// <summary>
-        /// Instance type.
-        /// </summary>
-        public Type InstanceType { get; }
-
-        #endregion
 
         #region Ctor
 
@@ -39,25 +20,19 @@ namespace CQELight.IoC
         /// Create a new type as type(s) registration for IoC container.
         /// </summary>
         /// <param name="registrationTypes">Registration types.</param>
-        public TypeRegistration(params Type[] registrationTypes)
+        public TypeRegistration(params Type[] registrationTypes) 
+            : base(typeof(T), registrationTypes)
         {
-            InstanceType = typeof(T);
-            _abstractionTypes = registrationTypes?.ToList() ?? new List<Type>();
-            _abstractionTypes.Add(InstanceType);
         }
+
 
         /// <summary>
         /// Create a new type as everything possible (self + all interfaces).
         /// </summary>
         /// <param name="forEverything">Flag that indicates if should register has everything possible.</param>
-        public TypeRegistration(bool forEverything)
+        public TypeRegistration(bool forEverything) : 
+            base(typeof(T), forEverything)
         {
-            InstanceType = typeof(T);
-            if(forEverything)
-            {
-                _abstractionTypes.Add(typeof(T));
-                _abstractionTypes.AddRange(typeof(T).GetInterfaces());
-            }
         }
 
         #endregion
