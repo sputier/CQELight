6ed9e1baba8915e585bc10ece2f5123060c09378
cdie <christophe.mommer@live.fr>
2019-02-03 10:24:35 +0100

    creation of generic type parameter + perf enhancements

---
+++ src/CQELight/IoC/TypeResolverParameter`.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQELight.IoC
+{
+    /// <summary>
+    /// Generic type resolver parameter.
+    /// </summary>
+    /// <typeparam name="T">Parameter type.</typeparam>
+    public class TypeResolverParameter<T> : TypeResolverParameter
+    {
+
+        #region Ctor
+
+        /// <summary>
+        /// Creates a new TypeResolverParameter with generic parameter type.
+        /// </summary>
+        /// <param name="value">Value of parameter.</param>
+        public TypeResolverParameter(T value)
+            : base(typeof(T), value)
+        {
+        }
+
+        #endregion
+    }
+}
