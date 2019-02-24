c7f6677d2c2ed009d051df351459e32ecb5dfb89
stauweak <chamfort.benjamin@gmail.com>
2019-02-24 14:31:38 +0100

    Implementation greg young's event store

--- src/CDELight.EventStore.GregYoungsEventStore/GYEventStore.cs
+++ src/CDELight.EventStore.GregYoungsEventStore/GYEventStore.cs
@@ -3,6 +3,7 @@ using CQELight.Abstractions.EventStore;
 using CQELight.Abstractions.EventStore.Interfaces;
 using CQELight.EventStore.GregYoungsEventStore;
 using CQELight.EventStore.GregYoungsEventStore.Common;
+using CQELight.Tools.Extensions;
 using EventStore.ClientAPI;
 using Newtonsoft.Json;
 using System;
@@ -25,27 +26,27 @@ namespace CDELight.EventStore.GregYoungsEventStore
 
         #region IEventStore implementation
 
-        public async Task<IAsyncEnumerable<IDomainEvent>> GetEventsFromAggregateIdAsync<TAggregate, TId>(TId aggregateUniqueId) where TAggregate : EventSourcedAggregate<TId>
+        public Task<IAsyncEnumerable<IDomainEvent>> GetEventsFromAggregateIdAsync<TAggregate, TId>(TId aggregateUniqueId) where TAggregate : EventSourcedAggregate<TId>
+        => GetEventsFromAggregateIdAsync(aggregateUniqueId, typeof(TAggregate));
+    
+        public async Task<IAsyncEnumerable<IDomainEvent>> GetEventsFromAggregateIdAsync<TId>(TId aggregateUniqueId, Type aggregateType)
         {
-            var events = new List<RecordedEvent>();
+            var events = new List<IDomainEvent>();
             var index = 0;
             bool allEventsRead = false;
             while (allEventsRead)
             {
-                var result = await EventStoreManager.Connection.ReadStreamEventsForwardAsync(aggregateUniqueId.ToString(), index, 1000,false);
-                if (!result.Events.Any())
+                var result = await EventStoreManager.Connection.ReadStreamEventsForwardAsync(aggregateUniqueId.ToString(), index, 1000, false).ConfigureAwait(false);
+                if (result.Events.Length == 0)
                 {
                     allEventsRead = true;
                 }
-                events.AddRange(result.Events.Select(x => x.Event));
+                events.AddRange(result.Events.Select(x => GetRehydratedEventFromDbEvent(x.Event)));
                 index = 1000;
             }
-        }
 
-        public Task<IAsyncEnumerable<IDomainEvent>> GetEventsFromAggregateIdAsync<TId>(TId aggregateUniqueId, Type aggregateType)
-        {
-            throw new NotImplementedException();
-        }        
+            return events.ToAsyncEnumerable();
+        }
 
         public Task StoreDomainEventAsync(IDomainEvent @event)
         {
@@ -66,17 +67,34 @@ namespace CDELight.EventStore.GregYoungsEventStore
         #region IAggregateEventStore implementation
 
 
-        public Task<IEventSourcedAggregate> GetRehydratedAggregateAsync<TId>(TId aggregateUniqueId, Type aggregateType)
+        public async Task<IEventSourcedAggregate> GetRehydratedAggregateAsync<TId>(TId aggregateUniqueId, Type aggregateType)
         {
-            throw new NotImplementedException();
+            var events = await (await GetEventsFromAggregateIdAsync(aggregateUniqueId, aggregateType).ConfigureAwait(false)).ToList().ConfigureAwait(false);
+
+            if (!(aggregateType.CreateInstance() is IEventSourcedAggregate aggregate))
+            {
+                throw new InvalidOperationException("EFEventStore.GetRehydratedAggregateAsync() : Cannot create a new instance of" +
+                    $" {aggregateType.FullName} aggregate. It should have one parameterless constructor (can be private).");
+            }
+
+            aggregate.RehydrateState(events);
+            return aggregate;
         }
 
-        public Task<TAggregate> GetRehydratedAggregateAsync<TAggregate, TId>(TId aggregateUniqueId) where TAggregate : EventSourcedAggregate<TId>, new()
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<TAggregate> GetRehydratedAggregateAsync<TAggregate, TId>(TId aggregateUniqueId) where TAggregate : EventSourcedAggregate<TId>, new()
+        => (await GetRehydratedAggregateAsync(aggregateUniqueId, typeof(TAggregate)).ConfigureAwait(false)) as TAggregate;
 
         #endregion
 
+
+        #region privates methods
+
+        private IDomainEvent GetRehydratedEventFromDbEvent(RecordedEvent evt)
+        {
+            var evtType = Type.GetType(evt.EventType);
+            return Encoding.UTF8.GetString(evt.Data).FromJson(evtType) as IDomainEvent;
+        }
+
+        #endregion
     }
 }
