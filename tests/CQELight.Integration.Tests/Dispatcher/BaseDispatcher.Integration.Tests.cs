d3bd8b1b7bf10486d236c0fc3da9971be988d750
cdie <christophe.mommer@gmail.com>
2019-03-01 12:16:10 +0100

    respect events order when publishing range

--- tests/CQELight.Integration.Tests/Dispatcher/BaseDispatcher.Integration.Tests.cs
+++ tests/CQELight.Integration.Tests/Dispatcher/BaseDispatcher.Integration.Tests.cs
@@ -1,6 +1,7 @@
 ﻿using CQELight.Abstractions.CQS.Interfaces;
 using CQELight.Abstractions.DDD;
 using CQELight.Abstractions.Events;
+using CQELight.Abstractions.Events.Interfaces;
 using CQELight.Buses.InMemory.Commands;
 using CQELight.Buses.InMemory.Events;
 using CQELight.Dispatcher;
@@ -59,6 +60,133 @@ namespace CQELight.Integration.Tests.Dispatcher
 
             loggerMock.Verify(m => m.Log(LogLevel.Warning, It.IsAny<EventId>(), It.IsAny<FormattedLogValues>(), It.IsAny<Exception>(), It.IsAny<Func<object, Exception, string>>()), Times.Once);
             coreDispatcherCalledWithoutSecurityCritical.Should().BeTrue();
+        }
+
+        private class DataHolder
+        {
+            public Dictionary<Guid, string> Data { get; set; }
+            = new Dictionary<Guid, string>();
+        }
+        private class EventOne : BaseDomainEvent
+        {
+            public EventOne(DataHolder data, Guid aggId)
+            {
+                Data = data;
+                AggregateId = aggId;
+            }
+
+            public DataHolder Data { get; set; }
+        }
+        private class EventTwo : BaseDomainEvent
+        {
+            public EventTwo(DataHolder data, Guid aggId)
+            {
+                Data = data;
+                AggregateId = aggId;
+            }
+
+            public DataHolder Data { get; set; }
+        }
+        private class EventThree : BaseDomainEvent
+        {
+            public EventThree(DataHolder data, Guid aggId)
+            {
+                Data = data;
+                AggregateId = aggId;
+            }
+
+            public DataHolder Data { get; set; }
+        }
+
+        private class EventOneHandler : IDomainEventHandler<EventOne>
+        {
+            public Task<Result> HandleAsync(EventOne domainEvent, IEventContext context = null)
+            {
+                if (domainEvent.Data.Data.ContainsKey((Guid)domainEvent.AggregateId))
+                {
+                    domainEvent.Data.Data[(Guid)domainEvent.AggregateId] += "1";
+                }
+                else
+                {
+                    domainEvent.Data.Data.Add((Guid)domainEvent.AggregateId, "1");
+                }
+                return Task.FromResult(Result.Ok());
+            }
+        }
+        private class EventTwoHandler : IDomainEventHandler<EventTwo>
+        {
+            public async Task<Result> HandleAsync(EventTwo domainEvent, IEventContext context = null)
+            {
+                await Task.Delay(10);
+                if (domainEvent.Data.Data.ContainsKey((Guid)domainEvent.AggregateId))
+                {
+                    domainEvent.Data.Data[(Guid)domainEvent.AggregateId] += "2";
+                }
+                else
+                {
+                    domainEvent.Data.Data.Add((Guid)domainEvent.AggregateId, "2");
+                }
+                return Result.Ok();
+            }
+        }
+        private class EventThreeHandler : IDomainEventHandler<EventThree>
+        {
+            public Task<Result> HandleAsync(EventThree domainEvent, IEventContext context = null)
+            {
+                if (domainEvent.Data.Data.ContainsKey((Guid)domainEvent.AggregateId))
+                {
+                    domainEvent.Data.Data[(Guid)domainEvent.AggregateId] += "3";
+                }
+                else
+                {
+                    domainEvent.Data.Data.Add((Guid)domainEvent.AggregateId, "3");
+                }
+                return Task.FromResult(Result.Ok());
+            }
+        }
+
+        [Fact]
+        public async Task PublishEventRange_NoSequence_Should_Respect_Order_SameAggregateId()
+        {
+            var aggId = Guid.NewGuid();
+            var data = new DataHolder();
+            var events = new Queue<IDomainEvent>();
+            events.Enqueue(new EventOne(data, aggId));
+            events.Enqueue(new EventTwo(data, aggId));
+            events.Enqueue(new EventThree(data, aggId));
+
+            var builder = new DispatcherConfigurationBuilder();
+            builder
+                .ForAllEvents()
+                .UseBus<InMemoryEventBus>();
+            var dispatcher = new BaseDispatcher(builder.Build());
+
+            await dispatcher.PublishEventsRangeAsync(events);
+            data.Data[aggId].Should().Be("123");
+        }
+
+        [Fact]
+        public async Task PublishEventRange_WithSequence_Should_Respect_Order_SameAggregateId()
+        {
+            var aggId = Guid.NewGuid();
+            var aggId2 = Guid.NewGuid();
+            var data = new DataHolder();
+            var events = new Queue<IDomainEvent>();
+            events.Enqueue(new EventOne(data, aggId) { Sequence = 1 });
+            events.Enqueue(new EventOne(data, aggId2) { Sequence = 1 });
+            events.Enqueue(new EventTwo(data, aggId) { Sequence = 2 });
+            events.Enqueue(new EventTwo(data, aggId2) { Sequence = 1 });
+            events.Enqueue(new EventThree(data, aggId) { Sequence = 3 });
+
+            var builder = new DispatcherConfigurationBuilder();
+            builder
+                .ForAllEvents()
+                .UseBus<InMemoryEventBus>();
+            var dispatcher = new BaseDispatcher(builder.Build());
+
+            await dispatcher.PublishEventsRangeAsync(events);
+            data.Data[aggId].Should().Be("123");
+            data.Data[aggId2].Should().Be("12");
         }
 
         #endregion
