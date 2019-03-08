0c2cdd28f3eb30ff3f2631a32d5a943bf8ad9ca1
cdie <christophe.mommer@gmail.com>
2019-03-08 11:41:55 +0100

    fixing benchmarks compilation issues

--- benchmarks/CQELight_Benchmarks/Benchmarks/EventStore/EFCore_EventStoreBenchmark.cs
+++ benchmarks/CQELight_Benchmarks/Benchmarks/EventStore/EFCore_EventStoreBenchmark.cs
@@ -1,18 +1,17 @@
 ﻿using BenchmarkDotNet.Attributes;
-using CQELight;
 using CQELight.Abstractions.EventStore.Interfaces;
+using CQELight.Abstractions.EventStore.Interfaces.Snapshots;
 using CQELight.EventStore;
 using CQELight.EventStore.EFCore;
 using CQELight.EventStore.EFCore.Common;
 using CQELight.EventStore.EFCore.Models;
-using CQELight.EventStore.EFCore.Snapshots;
+using CQELight.EventStore.Snapshots;
 using CQELight_Benchmarks.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CQELight_Benchmarks.Benchmarks
@@ -58,7 +57,7 @@ namespace CQELight_Benchmarks.Benchmarks
         {
             CreateDatabase(DatabaseType);
             CleanDatabases();
-            StoreNDomainEvents(new BasicSnapshotBehaviorProvider(new Dictionary<Type, IGenericSnapshotBehavior>()
+            StoreNDomainEvents(new BasicSnapshotBehaviorProvider(new Dictionary<Type, ISnapshotBehavior>()
             {
                 {typeof(TestEvent), new NumericSnapshotBehavior( 10) }
             }));
@@ -186,8 +185,10 @@ namespace CQELight_Benchmarks.Benchmarks
         {
             var store = new EFEventStore(
                 GetConfig(
-                    new BasicSnapshotBehaviorProvider(new Dictionary<Type, IGenericSnapshotBehavior>()
-                    { {typeof(TestEvent), new NumericSnapshotBehavior(10,GetDbOptions()) }}),
+                    new BasicSnapshotBehaviorProvider(new Dictionary<Type, ISnapshotBehavior>()
+                    {
+                        { typeof(TestEvent), new NumericSnapshotBehavior(10) }
+                    }),
                     bufferInfo: useBuffer ? BufferInfo.Default : BufferInfo.Disabled));
             for (int i = 0; i < numberEvents; i++)
             {
@@ -218,8 +219,11 @@ namespace CQELight_Benchmarks.Benchmarks
         public async Task RehydrateAggregate_WithSnapshot()
         {
             var store = new EFEventStore(
-                GetConfig(new BasicSnapshotBehaviorProvider(new Dictionary<Type, IGenericSnapshotBehavior>()
-                    { {typeof(TestEvent), new NumericSnapshotBehavior(10, GetDbOptions()) }})));
+                GetConfig(new BasicSnapshotBehaviorProvider(
+                    new Dictionary<Type, ISnapshotBehavior>()
+                    {
+                        { typeof(TestEvent), new NumericSnapshotBehavior(10) }
+                    })));
             var agg = await store.GetRehydratedAggregateAsync<TestAggregate, Guid>(AggregateId);
 
         }
