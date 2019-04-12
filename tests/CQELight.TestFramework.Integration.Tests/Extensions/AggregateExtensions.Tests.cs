0af8bf119e7b4224729a18598d2fc8dec6775273
cdie <christophe.mommer@gmail.com>
2019-04-12 15:31:19 +0200

    add some unit tests to tes framework

---
+++ tests/CQELight.TestFramework.Integration.Tests/Extensions/AggregateExtensions.Tests.cs
@@ -0,0 +1,39 @@
+﻿using CQELight.Abstractions.DDD;
+using CQELight.Abstractions.Events;
+using CQELight.TestFramework.Extensions;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CQELight.TestFramework.Integration.Tests.Extensions
+{
+    public class AggregateExtensionsTests : BaseUnitTestClass
+    {
+        #region ClearDomainEvents
+
+        private class TestEvent : BaseDomainEvent { }
+
+        private class Aggregate : AggregateRoot<Guid>
+        {
+            public void AddEvent() => AddDomainEvent(new TestEvent());
+        }
+
+        [Fact]
+        public void ClearDomainEvents_Should_Remove_All_AvailableEvents()
+        {
+            var a = new Aggregate();
+            a.AddEvent();
+
+            a.DomainEvents.Should().HaveCount(1);
+
+            a.ClearDomainEvents();
+
+            a.DomainEvents.Should().BeEmpty();
+        }
+
+        #endregion
+
+    }
+}
--- tests/CQELight.TestFramework.Integration.Tests/TestFrameworkAsyncActionAssertionTests.cs
+++ tests/CQELight.TestFramework.Integration.Tests/TestFrameworkAsyncActionAssertionTests.cs
@@ -1,7 +1,11 @@
 ﻿using CQELight.Abstractions.CQS.Interfaces;
 using CQELight.Abstractions.Dispatcher;
+using CQELight.Abstractions.Dispatcher.Interfaces;
+using CQELight.Abstractions.Events;
+using CQELight.Abstractions.Events.Interfaces;
 using CQELight.Dispatcher;
 using FluentAssertions;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +20,7 @@ namespace CQELight.TestFramework.Integration.Tests
 
         #region Ctor & members
 
+        private Mock<IDispatcher> _dispatcherMock = new Mock<IDispatcher>();
         private class MessageOne : IMessage { }
         private class MessageTwo : IMessage { }
 
@@ -150,6 +155,25 @@ namespace CQELight.TestFramework.Integration.Tests
             {
                 return CoreDispatcher.DispatchCommandAsync(new Cmd());
             }
+            public Task DispatchAsync_WithMock(IDispatcher mock)
+            {
+                return mock.DispatchCommandAsync(new Cmd());
+            }
+        }
+        class MultipleCommandTest
+        {
+            public async Task DispatchRangeAsync()
+            {
+                await CoreDispatcher.DispatchCommandAsync(new Cmd());
+                await CoreDispatcher.DispatchCommandAsync(new Cmd());
+                await CoreDispatcher.DispatchCommandAsync(new Cmd());
+            }
+            public async Task DispatchRangeAsync_WithMock(IDispatcher mock)
+            {
+                await mock.DispatchCommandAsync(new Cmd());
+                await mock.DispatchCommandAsync(new Cmd());
+                await mock.DispatchCommandAsync(new Cmd());
+            }
         }
 
         [Fact]
@@ -157,10 +181,147 @@ namespace CQELight.TestFramework.Integration.Tests
         {
             var cmd = await Test.WhenAsync(() => new CommandTest().DispatchAsync()).ThenCommandIsDispatched<Cmd>();
             cmd.Should().NotBeNull();
+
+            _dispatcherMock = new Mock<IDispatcher>();
+            var cmd2 = await Test.WhenAsync(() =>
+                    new CommandTest().DispatchAsync_WithMock(_dispatcherMock.Object), _dispatcherMock)
+                    .ThenCommandIsDispatched<Cmd>();
+            cmd2.Should().NotBeNull();
+            _dispatcherMock.Verify(m => m.DispatchCommandAsync(It.IsAny<ICommand>(), It.IsAny<ICommandContext>(), It.IsAny<string>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task ThenCommandIsDispatched_Should_Throw_If_NoCommandIsDispatched()
+        {
+            await Assert.ThrowsAsync<TestFrameworkException>(() => Test.WhenAsync(() => Task.Delay(1)).ThenCommandIsDispatched<Cmd>());
+            await Assert.ThrowsAsync<TestFrameworkException>(() => Test.WhenAsync(() => Task.Delay(1)).ThenCommandsAreDispatched());
+        }
+
+
+        [Fact]
+        public async Task ThenNoCommandAreDispatched_AsExpected()
+        {
+            await Test.WhenAsync(() => Task.Delay(1)).ThenNoCommandAreDispatched();
+
+            _dispatcherMock = new Mock<IDispatcher>();
+            await Test.WhenAsync(() =>
+                    Task.Delay(1), _dispatcherMock)
+                    .ThenNoCommandAreDispatched();
+            _dispatcherMock.Verify(m => m.DispatchCommandAsync(It.IsAny<ICommand>(), It.IsAny<ICommandContext>(), It.IsAny<string>()), Times.Never());
+        }
+
+        [Fact]
+        public async Task ThenNoCommandAredispatch_Should_Throw_If_Command_IsDispatcher()
+        {
+            await Assert.ThrowsAsync<TestFrameworkException>(() => Test.WhenAsync(() => new CommandTest().DispatchAsync()).ThenNoCommandAreDispatched());
+
+            _dispatcherMock = new Mock<IDispatcher>();
+            await Assert.ThrowsAsync<TestFrameworkException>(() => Test.WhenAsync(() => new CommandTest().DispatchAsync_WithMock(_dispatcherMock.Object), _dispatcherMock).ThenNoCommandAreDispatched());
+        }
+
+        [Fact]
+        public async Task ThenCommandAreDispatched_AsExpected()
+        {
+            var cmds = await Test.WhenAsync(() => new MultipleCommandTest().DispatchRangeAsync()).ThenCommandsAreDispatched();
+            cmds.Should().NotBeNull();
+            cmds.Should().HaveCount(3);
+
+            _dispatcherMock = new Mock<IDispatcher>();
+            var cmds2 = await Test.WhenAsync(() =>
+                     new MultipleCommandTest().DispatchRangeAsync_WithMock(_dispatcherMock.Object), _dispatcherMock)
+                    .ThenCommandsAreDispatched();
+            cmds2.Should().NotBeNull();
+            cmds.Should().HaveCount(3);
+            _dispatcherMock.Verify(m => m.DispatchCommandAsync(It.IsAny<ICommand>(), It.IsAny<ICommandContext>(), It.IsAny<string>()), Times.Exactly(3));
         }
 
         #endregion
 
+        #region Event Dispatch
+
+        class Evt : BaseDomainEvent { }
+
+        class EventTest
+        {
+            public Task PublishAsync()
+            {
+                return CoreDispatcher.PublishEventAsync(new Evt());
+            }
+            public Task PublishAsync_WithMock(IDispatcher mock)
+            {
+                return mock.PublishEventAsync(new Evt());
+            }
+        }
+
+        class MultipleEventTest
+        {
+            public async Task PublishEventRangeAsync()
+            {
+                await CoreDispatcher.PublishEventAsync(new Evt());
+                await CoreDispatcher.PublishEventAsync(new Evt());
+                await CoreDispatcher.PublishEventAsync(new Evt());
+            }
+            public async Task PublishEventRangeAsync_WithMock(IDispatcher mock)
+            {
+                await mock.PublishEventAsync(new Evt());
+                await mock.PublishEventAsync(new Evt());
+                await mock.PublishEventAsync(new Evt());
+            }
+        }
+
+        [Fact]
+        public async Task ThenEventShouldBeRaised_AsExpected()
+        {
+            var evt = await Test.WhenAsync(() => new EventTest().PublishAsync()).ThenEventShouldBeRaised<Evt>();
+            evt.Should().NotBeNull();
+
+            _dispatcherMock = new Mock<IDispatcher>();
+            var evt2 = await Test.WhenAsync(() =>
+                new EventTest().PublishAsync_WithMock(_dispatcherMock.Object), _dispatcherMock).ThenEventShouldBeRaised<Evt>();
+            evt2.Should().NotBeNull();
+            _dispatcherMock.Verify(m => m.PublishEventAsync(It.IsAny<IDomainEvent>(), It.IsAny<IEventContext>(), It.IsAny<string>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task ThenEventShouldBeRaised_Should_Throw_IfNoEvent_Is_Raised()
+        {
+            await Assert.ThrowsAsync<TestFrameworkException>(() => Test.WhenAsync(() => Task.Delay(1)).ThenEventShouldBeRaised<Evt>());
+            await Assert.ThrowsAsync<TestFrameworkException>(() => Test.WhenAsync(() => Task.Delay(1)).ThenEventsShouldBeRaised());
+        }
+
+        [Fact]
+        public async Task ThenNoEventShouldBeRaised_AsExpected()
+        {
+            await Test.WhenAsync(() => Task.Delay(1)).ThenNoEventShouldBeRaised();
+
+            _dispatcherMock = new Mock<IDispatcher>();
+            await Test.WhenAsync(() => Task.Delay(1), _dispatcherMock).ThenNoEventShouldBeRaised();
+        }
+
+        [Fact]
+        public async Task ThenNoEventShouldBeRaised_Should_Throw_If_No_Event_AreRaised()
+        {
+            await Assert.ThrowsAsync<TestFrameworkException>(() => Test.WhenAsync(() => new EventTest().PublishAsync()).ThenNoEventShouldBeRaised());
+
+            _dispatcherMock = new Mock<IDispatcher>();
+            await Assert.ThrowsAsync<TestFrameworkException>(() => Test.WhenAsync(() => new EventTest().PublishAsync_WithMock(_dispatcherMock.Object), _dispatcherMock).ThenNoEventShouldBeRaised());
+        }
+
+        [Fact]
+        public async Task ThenEventsShouldBeRaised_AsExpected()
+        {
+            var cmds = await Test.WhenAsync(() => new MultipleEventTest().PublishEventRangeAsync()).ThenEventsShouldBeRaised();
+            cmds.Should().NotBeNull();
+            cmds.Should().HaveCount(3);
+
+            _dispatcherMock = new Mock<IDispatcher>();
+            var cmds2 = await Test.WhenAsync(() => new MultipleEventTest().PublishEventRangeAsync_WithMock(_dispatcherMock.Object), _dispatcherMock).ThenEventsShouldBeRaised();
+            cmds2.Should().NotBeNull();
+            cmds2.Should().HaveCount(3);
+            _dispatcherMock.Verify(m => m.PublishEventAsync(It.IsAny<IDomainEvent>(), It.IsAny<IEventContext>(), It.IsAny<string>()), Times.Exactly(3));
+        }
+
+        #endregion
 
     }
 }
